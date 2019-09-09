de513074fc7bba4c9e529838e2a3b6cb33a1dba4
kattakatao <kattakatao1018@gmail.com>
2019-09-09 08:00:49 +0000

    Gemを追加しました

---
+++ db/schema.rb
@@ -0,0 +1,136 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_09_09_061640) do
+
+  create_table "arrivals", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "arrival_count"
+    t.date "purchase_order_date"
+    t.date "arrival_expected_date"
+    t.integer "arrival_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "artist_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "end_user_id"
+    t.integer "item_count"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "delivery_adresses", force: :cascade do |t|
+    t.integer "end_user_id"
+    t.string "addressee"
+    t.text "postal_code"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "address"
+  end
+
+  create_table "discs", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "genre_id"
+    t.integer "item_id"
+    t.string "disc_name"
+    t.integer "disc_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "gacket_images", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "jacket_number"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "label_id"
+    t.string "item_name"
+    t.decimal "list_price"
+    t.decimal "cost_price"
+    t.string "sales_status"
+    t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "label_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.string "item_name"
+    t.integer "item_count"
+    t.decimal "list_price"
+    t.decimal "tax_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "end_user_id"
+    t.string "addressee"
+    t.string "postal_code"
+    t.text "address"
+    t.string "phone_number"
+    t.string "payment_methods"
+    t.decimal "subtotal"
+    t.decimal "shipping_fee"
+    t.decimal "grand_total"
+    t.integer "delivery_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shipping_fees", force: :cascade do |t|
+    t.integer "shipping_fee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.integer "disc_id"
+    t.integer "recording_number"
+    t.string "song_title"
+    t.time "play_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tax_rates", force: :cascade do |t|
+    t.decimal "tax_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
