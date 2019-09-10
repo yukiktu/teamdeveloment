2aa274953c787416d125019a8e9ab37539f0bb94
yukiktu <yuuki0421ktu@gmail.com>
2019-09-10 20:28:20 +0900

    orderコントローラー直した

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,37 @@
+class OrdersController < ApplicationController
+
+	def index
+		if currentuser.id == admin_users.id
+			@order = Order.all
+			@order_items = Order_item.where(order_id: Order.id)
+			@s_option = Order.delivery_status
+		elsif currentuser.present?
+			@order = Order.find(currentuser.id)
+			@order_items = Order_item.where(order_id: Order.id)
+		else
+			flash[:notice] = "nothing user"
+			redirect_to index
+		end
+	end
+
+	def new
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	def create
+
+	end
+
+	private
+	def order_params
+		params
+
+end
