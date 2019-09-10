5646ca304ee11d18137ed90b9f419d35cb284104
ymmt1089 <ymmt1089@gmail.com>
2019-09-10 21:42:48 +0900

    一旦変更を保存します

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 		end_user_path()
 	end
 
-	before_action :configure_permitted_parameters, if: :devise_controller?
+		before_action :configure_permitted_parameters, if: :devise_controller?
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
