fce71b08a907bbf56a36da4a26f903dc65173866
yukiktu <yuuki0421ktu@gmail.com>
2019-09-11 09:21:54 +0900

    カート画面のhtml記述しました。

---
+++ db/migrate/20190910202934_add_default_to_cart_items.rb
@@ -0,0 +1,8 @@
+class AddDefaultToCartItems < ActiveRecord::Migration[5.2]
+	def change
+		
+    	change_column_null :cart_items, :end_user_id, false
+		change_column_null :cart_items, :item_count, 1, false
+    	change_column_null :cart_items, :item_id, false
+  	end
+end
