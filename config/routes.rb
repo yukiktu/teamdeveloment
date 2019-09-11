d04e84d54f920efe35b7fb3d61ec7faae0520a92
yukiktu <yuuki0421ktu@gmail.com>
2019-09-11 09:23:46 +0900

    routes(仮)です。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users
   devise_for :end_users
+  get '/cart_items/', to: 'cart_items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
