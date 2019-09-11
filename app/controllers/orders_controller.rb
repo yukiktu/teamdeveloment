4b850b0b6bdc09d34f2ba6fabe53b2a584325761
yukiktu <yuuki0421ktu@gmail.com>
2019-09-11 16:47:10 +0900

    お支配選択実装中

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,10 @@ class OrdersController < ApplicationController
 	end
 
 	def new
-
+		@payment_methods = Order.new(order_params)
+		@payment_methods.end_user_id = currentuser.id
+		@addressee = Delivery_address.new(delivery_address_params)
+		@addressee.end_user_id = currentuser.id
 	end
 
 	def update
@@ -23,16 +26,22 @@ class OrdersController < ApplicationController
 	end
 
 	def destroy
-
+		@payment_methods.delete
+		#@addressee.delete
 	end
 
 	def create
-
+		@payment_methods.save
+		@addressee.save
 	end
 
 	private
 	def order_params
-		params
+		params.require(:order).permit(:end_user_id, :addressee, :postal_code, :adress, :phone_number, :payment_methods, :subtotal, :shipping_fee, :grand_total)
+	end
+	def delivery_address_params
+		params.require(:delivery_address).permit(:end_user_id, :addressee, :postal_code, :address, :phone_number)
+	end
 
 
 end
