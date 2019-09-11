c55f1e773c448c4bf62638876e18a57d82404550
yukiktu <yuuki0421ktu@gmail.com>
2019-09-12 04:58:01 +0900

    表追記

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,24 +1,32 @@
 class LabelsController < ApplicationController
 	def index
-		@label = Label.all
-		@song_title
+		@items = Item.all
+		#@artists = Artist.find(id: @items.artist)
+		arrival = Arrival.where(item_id: @items)
+		if arrival.present?
+			@arrivals = Arrival.where(item_id: @items)
+		else
+			@arrivals = Arrival.new
+			@arrivals.arrival_count = '0'
+			@arrivals.arrival_expected_date = "未納"
+		end
 	end
 
 
 	def new
 		@label = Label.new
 		@artist = Artist.new
-		@artist.label = @label
+		#@artist.label = @label
 		@item = Item.new
-		@item.artist = @artist
+		#@item.artist = @artist
 		@disc = Disc.new
-		@disc.item = @item
+		#@disc.item = @item
 		@song = Song.new
-		@song.disc = @disc
+		#@song.disc = @disc
 		@genre = Genre.new
-		@disc.genre = @genre
+		#@disc.genre = @genre
 		@gacket_image = GacketImage.new
-		@gacket_image.item = @item
+		#@gacket_image.item = @item
 
 	end
 
@@ -26,15 +34,21 @@ class LabelsController < ApplicationController
 		@label = Label.new(label_params)
 		@genre = Genre.new(genre_params)
 		@artist = Artist.new(artist_params)
-		@items = Item.new(item_params)
+		@item = Item.new(item_params)
 		#@gacket_image = GacketImage.new(gacket_image_params)
 		@disc = Disc.new(disc_params)
 		@song = Song.new(song_params)
+		@artist.label = @label
+		@item.artist = @artist
+		@disc.item = @item
+		@song.disc = @disc
+		@disc.genre = @genre
+		#@gacket_image.item = @item
 
 		if @label.save && @song.save
 			@genre.save
 			@artist.save
-			@items.save
+			@item.save
 			#@gacket_image.save
 			@disc.save
 			redirect_to labels_path
@@ -51,7 +65,7 @@ class LabelsController < ApplicationController
 		params.require(:artist).permit(:artist_name, :label_id)
 	end
 	def item_params
-		params.require(:item).permit(:item_name, :list_price, :cost_price, :sales_status, :release_date,  gacket_images_images: [])
+		params.require(:item).permit(:artist_id, :item_name, :list_price, :cost_price, :sales_status, :release_date,  gacket_images_images: [])
 	end
 	def genre_params
 		params.require(:genre).permit(:genre_name)
