92b0e5b69d7cf434282b9a65c08c4af25e991c0b
kattakatao <kattakatao1018@gmail.com>
2019-09-11 08:36:03 +0000

    新規商品追加画面途中です2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,27 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
- end
+  end
 
   def new
     @item = Item.new
+    @item.discs.build
   end
 
   def create
    @item = Item.new(item_params)
-　　 @item.save
+   if @item.save
+
    redirect_to items_path
+
+  else
+   render:new
   end
+end
+
+   def show
+    @item = Item.find(params[:id])
+   end
 
 
 private
@@ -21,7 +31,7 @@ private
   def item_params
      params.require(:item).permit(
       :item_name, :list_price, :cost_price,
-      discs_attributes: [:genre_id, :item_id, :disc_name, :disc_number, :id, :artist_id],
+      discs_attributes: [:genre_id, :item_id, :disc_name, :disc_number, :id, :artist_id, :artist_name],
       songs_attributes: [:id, :disc_id, :recording_number, :song_title, :play_time, ],
       gacket_images_attributes: [:id, :item_id, :gacket_number, :image_id]
       )
