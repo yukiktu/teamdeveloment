d824b9f897ff6710b89cfdcafef0ff1289b96d06
kattakatao <kattakatao1018@gmail.com>
2019-09-12 09:50:00 +0000

    aaaa

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,84 +1,27 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-    #@artists = Artist.find(id: @items.artist)
-    arrival = Arrival.where(item_id: @items)
-    if arrival.present?
-      @arrivals = Arrival.where(item_id: @items)
-    else
-      @arrivals = Arrival.new
-      @arrivals.arrival_count = '0'
-      @arrivals.arrival_expected_date = ""
-    end
-  end
-
-
   def new
-    @label = Label.new
-    @artist = Artist.new
-    #@artist.label = @label
     @item = Item.new
-    #@item.artist = @artist
-    @disc = Disc.new
-    #@disc.item = @item
-    @song = Song.new
-    #@song.disc = @disc
-    @genre = Genre.new
-    #@disc.genre = @genre
-    @gacket_image = GacketImage.new
-    #@gacket_image.item = @item
-
+    @item.gacket_images.build
+    @disc = @item.discs.build
+    @song = @disc.songs.build
   end
 
   def create
-    @label = Label.new(label_params)
-    @genre = Genre.new(genre_params)
-    @artist = Artist.new(artist_params)
-    @item = Item.new(item_params)
-    #@gacket_image = GacketImage.new(gacket_image_params)
-    @disc = Disc.new(disc_params)
-    @song = Song.new(song_params)
-    @artist.label = @label
-    @item.artist = @artist
-    @disc.item = @item
-    @song.disc = @disc
-    @disc.genre = @genre
-    #@gacket_image.item = @item
+   @item = Item.new(item_params)
+　　@item.save
+  end
 
-    if @label.save && @song.save
-      @genre.save
-      @artist.save
-      @item.save
-      #@gacket_image.save
-      @disc.save
-      redirect_to labels_path
-    else
-      render 'new'
-    end
+  def about
   end
 
   private
-  def label_params
-    params.require(:label).permit(:label_name)
-  end
-  def artist_params
-    params.require(:artist).permit(:artist_name, :label_id)
-  end
+
   def item_params
-    params.require(:item).permit(:artist_id, :item_name, :list_price, :cost_price, :sales_status, :release_date,  gacket_images_images: [])
+     params.require(:item).permit(
+      :item_name, :list_price, :cost_price, :sales_status, :release_date, :jacket_number, gacket_images:[],
+      discs_attributes: [:id,:genre_id, :item_id, :disc_name, :disc_number, ],
+      songs_attributes: [:id, :disc_id, :recording_number, :song_title, :play_time, ],
+      )
   end
-  def genre_params
-    params.require(:genre).permit(:genre_name)
-  end
-  def disc_params
-    params.require(:disc).permit(:disc_name, :dis_number, :genre_id, :item_id)
-  end
-  def song_params
-    params.require(:song).permit(:record_number, :song_title, :play_time, :disc_id,)
-  end
-  #def gacket_image_params
-  # params.require(:gacket_image).require(:jacket_number)#, :image_id)
-  #end
 
 end
-
