f21b820425629adb4feae28a6d255154c264fb28
yukiktu <yuuki0421ktu@gmail.com>
2019-09-12 18:39:00 +0900

    コントロラー実装しました。htmlにエラーコメントとあります。cssも直してください。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
     #@artists = Artist.find(id: @items.artist)
+    #↑初期に作ったテーブルがnullなのでそれを削除すると使えます
     arrival = Arrival.where(item_id: @items)
     if arrival.present?
       @arrivals = Arrival.where(item_id: @items)
@@ -16,21 +17,14 @@ class ItemsController < ApplicationController
   def new
     @label = Label.new
     @artist = Artist.new
-    #@artist.label = @label
     @item = Item.new
-    #@item.artist = @artist
     @disc = Disc.new
-    #@disc.item = @item
     @song = Song.new
-    #@song.disc = @disc
     @genre = Genre.new
-    #@disc.genre = @genre
     @gacket_image = GacketImage.new
-    #@gacket_image.item = @item
-
   end
 
-  def create
+  def create #↓@gacket_imageは多分不要？paramsも
     @label = Label.new(label_params)
     @genre = Genre.new(genre_params)
     @artist = Artist.new(artist_params)
@@ -38,11 +32,14 @@ class ItemsController < ApplicationController
     #@gacket_image = GacketImage.new(gacket_image_params)
     @disc = Disc.new(disc_params)
     @song = Song.new(song_params)
-    @artist.label = @label
-    @item.artist = @artist
-    @disc.item = @item
+
     @song.disc = @disc
+    @disc.item = @item
     @disc.genre = @genre
+    @disc.artist = @artist
+    @item.label = @label
+    @artist.label = @label
+
     #@gacket_image.item = @item
 
     if @label.save && @song.save
@@ -62,16 +59,16 @@ class ItemsController < ApplicationController
     params.require(:label).permit(:label_name)
   end
   def artist_params
-    params.require(:artist).permit(:artist_name, :label_id)
+    params.require(:artist).permit(:artist_name)
   end
   def item_params
-    params.require(:item).permit(:artist_id, :item_name, :list_price, :cost_price, :sales_status, :release_date,  gacket_images_images: [])
+    params.require(:item).permit(:artist_id, :item_name, :list_price, :cost_price, :sales_status, :release_date, :label_id, gacket_images_images: [])
   end
   def genre_params
     params.require(:genre).permit(:genre_name)
   end
   def disc_params
-    params.require(:disc).permit(:disc_name, :dis_number, :genre_id, :item_id)
+    params.require(:disc).permit(:disc_name, :dis_number, :genre_id, :item_id, :genre_id, :artist_id)
   end
   def song_params
     params.require(:song).permit(:record_number, :song_title, :play_time, :disc_id,)
