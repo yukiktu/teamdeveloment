f6a715723af6b074e14b23cde1dfc5938415da07
ymmt1089 <ymmt1089@gmail.com>
2019-09-14 12:17:13 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users
   devise_for :end_users
   get '/cart_items/', to: 'cart_items#index'
-  resources :orders
+  # resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :end_users
+  resources :cart_items
+  resources :orders
+  resources :items
 end
