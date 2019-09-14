69fc4a9e07adb3a465ac41a701e812ead99cf0ec
yukiktu <yuuki0421ktu@gmail.com>
2019-09-14 15:58:47 +0900

    エラーなおした

--- db/migrate/20190910202934_add_default_to_cart_items.rb
+++ db/migrate/20190910202934_add_default_to_cart_items.rb
@@ -1,9 +1,5 @@
 class AddDefaultToCartItems < ActiveRecord::Migration[5.2]
 	def change
-<<<<<<< HEAD
-=======
-		
->>>>>>> 8749b72ecf2947188dfd4e8d997c695fff3ac350
     	change_column_null :cart_items, :end_user_id, false
 		change_column_null :cart_items, :item_count, 1, false
     	change_column_null :cart_items, :item_id, false
