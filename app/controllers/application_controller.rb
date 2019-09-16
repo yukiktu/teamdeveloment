090a9e249176328696cc4b79c0e8512a59c8abf1
yukiktu <yuuki0421ktu@gmail.com>
2019-09-16 21:16:01 +0900

    カート機能完了です。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
-		user_path(current_end_user)
+		items_path
 	end
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
