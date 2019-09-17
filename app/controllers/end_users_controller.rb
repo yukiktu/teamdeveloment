8e81818953d4005f265e4ac91b65b140ae42cd3c
yukiktu <yuuki0421ktu@gmail.com>
2019-09-17 11:12:14 +0900

    ざっくり住所選択画面、ざっくり購入確定画面、ざっくりマイページ　一度も動かしてません

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -1,12 +1,16 @@
 class EndUsersController < ApplicationController
-
-# before_action :authenticate_end_user!
-
-	def index
-	end
+before_action :authenticate_end_user!, only: [:show, :edit]
 
 	def show
+		if
 		@end_user = current_end_user
+		@orders = Oder.where(end_user_id: current_end_user.id)
+	end
+
+	def edit
+	end
+
+	def update
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_044327) do
+ActiveRecord::Schema.define(version: 2019_09_16_182018) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "yuuki0421ktu@gmail.com", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_044327) do
 
   create_table "arrivals", force: :cascade do |t|
     t.integer "item_id"
+    t.integer "artist_id"
     t.integer "arrival_count"
     t.date "purchase_order_date"
     t.date "arrival_expected_date"
@@ -144,6 +145,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_044327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "artist_name", null: false
+    t.integer "item_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -157,6 +159,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_044327) do
     t.decimal "shipping_fee", null: false
     t.decimal "grand_total", null: false
     t.integer "delivery_status", default: 0
+    t.string "artist_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["address"], name: "index_orders_on_address"
