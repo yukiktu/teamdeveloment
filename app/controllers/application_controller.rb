3864348ee489c499508ca1b352c60ed9cad6b023
yukiktu <yuuki0421ktu@gmail.com>
2019-09-25 14:44:59 +0900

    リダイレクト先追記しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	def after_sign_in_path_for(resource)
-		items_path
+		if cuurent_end_user_signed_in?
+			items_path
+		elsif current_admin_singned_in?
+			orders_path
+		end
+	end
+
+	def after_sign_out_path_for(resource)
+  		items_path
 	end
 
 
