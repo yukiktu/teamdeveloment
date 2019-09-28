aeb5090828aaf10fe43686581724bfcf16b13381
yukiktu <yuuki0421ktu@gmail.com>
2019-09-28 14:44:56 +0900

    javaspricktやめて

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
+//= require turbolinks
 //= require rails-ujs
 //= require activestorage
 //= require bootstrap-sprockets
